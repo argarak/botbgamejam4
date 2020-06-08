34afab5ad91ea23add06909dbf142f96447236c1
Argarak <kzer-za@cryptolab.net>
2020-06-08 23:44:38 +0100

    added icon drawing routines

--- main.js
+++ main.js
@@ -1,23 +1,60 @@
+class Graphics {
+  constructor() {
+    this.canvas = document.getElementById("gameCanvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.ctx.imageSmoothingEnabled = false;
+
+    this.spritesheet = document.getElementById("spritesheet");
+  }
+
+  drawIcon(iconName, x, y, scale = 1, rotate = 0, cx = -1, cy = -1) {
+    let icon = botbIcon.getIcon(iconName);
+    let xpos = cx;
+    let ypos = cy;
+
+    if (cx === -1) {
+      xpos = icon.width * scale / 2;
+    }
+
+    if (cy === -1) {
+      ypos = icon.height * scale / 2;
+    }
+
+    this.ctx.setTransform(scale, 0, 0, scale, x, y);
+    this.ctx.rotate(rotate * Math.PI / 180);
+
+    this.ctx.drawImage(
+      this.spritesheet,
+      icon.posX,
+      icon.posY,
+      icon.width,
+      icon.height,
+      -xpos,
+      -ypos,
+      icon.width * scale,
+      icon.height * scale
+    );
+
+    this.ctx.setTransform(1, 0, 0, 1, 0, 0);
+  }
+}
+
+let graphics = new Graphics();
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("dom loaded");
 
-  let canvas = document.getElementById("gameCanvas");
-  let ctx = canvas.getContext("2d");
+  var testRotate = 0;
+  var testScale = 0;
 
-  let spritesheet = document.getElementById("spritesheet");
+  function testAnim() {
+    testRotate += 2;
+    testScale = Math.sin(testRotate / 50) * 3;
 
-  let iconTest = botbIcon.getIcon("nsf");
+    graphics.drawIcon("pixel", 100, 100, testScale, testRotate);
 
-  ctx.drawImage(spritesheet, 33, 71, 104, 124, 21, 20, 87, 104);
-  ctx.drawImage(
-    spritesheet,
-    iconTest.posX,
-    iconTest.posY,
-    iconTest.width,
-    iconTest.height,
-    100,
-    100,
-    iconTest.width,
-    iconTest.height
-  );
+    window.requestAnimationFrame(testAnim);
+  }
+
+  testAnim();
 });
