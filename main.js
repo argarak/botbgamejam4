4e828c1a31f4aa32ceaccd745e45cd29dfa4d3e8
Argarak <kzer-za@cryptolab.net>
2020-06-09 19:00:22 +0100

    added player movement

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ class Graphics {
     this.ctx.imageSmoothingEnabled = false;
 
     this.spritesheet = document.getElementById("spritesheet");
+    this.testIcon = botbIcon.random16icon();
+
+    this.lightsources = [
+      {
+        x: 0,
+        y: 0,
+        intensity: 10,
+        color: [25, 250, 20]
+      }
+    ];
   }
 
   clearScreen() {
@@ -45,35 +55,193 @@ class Graphics {
 
     this.ctx.setTransform(1, 0, 0, 1, 0, 0);
   }
+
+  drawMap() {
+    let maptest = [
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+
+    for (let rowindex = 0; rowindex < maptest.length; rowindex++) {
+      for (
+        let tileindex = 0;
+        tileindex < maptest[rowindex].length;
+        tileindex++
+      ) {
+        switch (maptest[rowindex][tileindex]) {
+          case 1:
+            this.drawIcon(
+              this.testIcon,
+              16 + tileindex * 16,
+              16 + rowindex * 16,
+              1,
+              0,
+              -1,
+              -1
+            );
+            break;
+          case 0:
+            this.drawIcon(
+              this.testIcon,
+              16 + tileindex * 16,
+              16 + rowindex * 16,
+              1,
+              0,
+              -1,
+              -1,
+              "rgba(0,0,0,0.8)"
+            );
+            break;
+        }
+      }
+    }
+
+    //this.drawIcon(botbIcon.random16icon(), 0, 0, 1);
+  }
 }
 
-let graphics = new Graphics();
+var graphics = new Graphics();
+
+class Player {
+  constructor() {
+    this.xpos = 16 * 4;
+    this.ypos = 16 * 4;
+    this.icon = botbIcon.getIcon("n00b");
+
+    // player movement
+    this.speedX = 0;
+    this.speedY = 0;
+    this.acceleration = 5;
+    this.friction = 1;
+    this.maxSpeedLimit = 20;
+    this.minSpeedLimit = -this.maxSpeedLimit;
+
+    this.movingUp = false;
+    this.movingDown = false;
+    this.movingLeft = false;
+    this.movingRight = false;
+
+    this.debug = true;
+  }
+
+  updatePositions() {
+    if (this.debug) {
+      document.getElementById("speedX").innerHTML =
+        "player.speedX = " + this.speedX;
+    }
+
+    if (this.movingDown) {
+      if (this.speedY < this.maxSpeedLimit) {
+        this.speedY += this.acceleration;
+      }
+    } else {
+      if (this.speedY > 0) {
+        this.speedY -= this.friction;
+      }
+    }
+
+    if (this.movingUp) {
+      if (this.speedY > this.minSpeedLimit) {
+        this.speedY -= this.acceleration;
+      }
+    } else {
+      if (this.speedY < 0) {
+        this.speedY += this.friction;
+      }
+    }
+
+    if (this.movingLeft) {
+      if (this.speedX > this.minSpeedLimit) {
+        this.speedX -= this.acceleration;
+      }
+    } else {
+      if (this.speedX < 0) {
+        this.speedX += this.friction;
+      }
+    }
+
+    if (this.movingRight) {
+      if (this.speedX < this.maxSpeedLimit) {
+        this.speedX += this.acceleration;
+      }
+    } else {
+      if (this.speedX > 0) {
+        this.speedX -= this.friction;
+      }
+    }
+
+    if (this.xpos > 0) {
+      this.xpos += this.speedX / 10;
+    }
+
+    if (this.ypos > 0) {
+      this.ypos += this.speedY / 10;
+    }
+  }
+
+  draw() {
+    this.updatePositions();
+    graphics.drawIcon(this.icon, this.xpos, this.ypos);
+  }
+}
+
+var player = new Player();
+
+document.addEventListener("keydown", function(event) {
+  if (event.code === "KeyW") {
+    player.movingUp = true;
+  }
+
+  if (event.code === "KeyA") {
+    player.movingLeft = true;
+  }
+
+  if (event.code === "KeyS") {
+    player.movingDown = true;
+  }
+
+  if (event.code === "KeyD") {
+    player.movingRight = true;
+  }
+});
+
+document.addEventListener("keyup", function(event) {
+  if (event.code === "KeyW") {
+    player.movingUp = false;
+  }
+
+  if (event.code === "KeyA") {
+    player.movingLeft = false;
+  }
+
+  if (event.code === "KeyS") {
+    player.movingDown = false;
+  }
+
+  if (event.code === "KeyD") {
+    player.movingRight = false;
+  }
+});
 
 document.addEventListener("DOMContentLoaded", () => {
-  var testRotate = 0;
-  var testScale = 0;
-  var tintTest = 0;
-
-  let randIcon = botbIcon.random16icon();
-
+  graphics.drawMap();
   function gameLoop() {
-    testRotate += 2;
-    testScale = 1 + Math.sin(testRotate / 50) * 3;
-
-    tintTest = Math.abs(Math.sin(testRotate / 50));
-
     graphics.clearScreen();
-    graphics.drawIcon(
-      randIcon,
-      200 + Math.sin(testRotate / 50) * 100,
-      200,
-      testScale,
-      testRotate,
-      -1,
-      -1,
-      "rgba(0,0,0, " + tintTest + ")"
-    );
-
+    graphics.drawMap();
+    player.draw();
     window.requestAnimationFrame(gameLoop);
   }
 
